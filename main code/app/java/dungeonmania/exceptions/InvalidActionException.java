e914500242f2ce9004d148e26fc9055f6c99fdc1
NidhoggMegaCrop <nidhoggaus@gmail.com>
2025-11-14 19:46:02 +1100

    Delete main code/app/java directory

--- main code/app/java/dungeonmania/exceptions/InvalidActionException.java
+++
@@ -1,8 +0,0 @@
-package dungeonmania.exceptions;
-
-/** Thrown when a player attempts to interact with something they can't interact with. */
-public class InvalidActionException extends Exception {
-    public InvalidActionException(String message) {
-        super(message);
-    }
-}
